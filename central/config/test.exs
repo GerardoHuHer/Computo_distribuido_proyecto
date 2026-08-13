1e30626a760e383a588ba10540a575c00b956077
Gerardo <gerardohuerta1502@gmail.com>
2026-08-12 23:50:07 -0600

    feat: Configuración de la base de datos para los test

--- central/config/test.exs
+++ central/config/test.exs
@@ -6,10 +6,10 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :central, Central.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "central_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  hostname: System.get_env("DB_HOST"),
+  database: "central_test",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
