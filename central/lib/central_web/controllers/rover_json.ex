4fb987e41c167ca27e9bdf04430c6198ff34b44d
Gerardo <gerardohuerta1502@gmail.com>
2026-08-12 23:10:17 -0600

    feat: View que se devolverá al frontend
    Como se parsea a json para devolverlo al frontend

---
+++ central/lib/central_web/controllers/rover_json.ex
@@ -0,0 +1,25 @@
+defmodule CentralWeb.RoverJson do
+  alias Central.Rover.Rover
+
+  def show(%{rover: rover}) do
+    %{data: data(rover)}
+  end
+
+  def error(%{changeset: changeset}) do
+    %{errors: Ecto.Changeset.traverse_errors(changeset, &translate_error/1)}
+  end
+
+  defp data(%Rover{} = rover) do
+    %{
+      id: rover.id,
+      pos_x: rover.pos_x,
+      pos_y: rover.pos_y
+    }
+  end
+
+  defp translate_error({msg, opts}) do
+    Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+      opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+    end)
+  end
+end
---
+++ central/lib/central_web/controllers/rover_mejora_json.ex
@@ -0,0 +1,28 @@
+defmodule CentralWeb.RoverMejoraJson do
+  # 
+  alias Central.Rover.RoverMejora
+
+  def show(%{mejora: mejora}) do
+    %{data: data(mejora)}
+  end
+
+  def error(%{changeset: changeset}) do
+    %{errors: Ecto.Changeset.traverse_errors(changeset, &translate_error/1)}
+  end
+
+  defp data(%RoverMejora{} = mejora) do
+    %{
+      id: mejora.id,
+      name: mejora.name,
+      description: mejora.description,
+      puntaje: mejora.puntaje,
+      id_rover: mejora.id_rover
+    }
+  end
+
+  defp translate_error({msg, opts}) do
+    Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+      opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+    end)
+  end
+end
