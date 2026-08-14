90be331cf1ca04e68c38855b18b354b5be624be3
Gerardo <gerardohuerta1502@gmail.com>
2026-08-13 20:37:14 -0600

    feat: Funciones para actualizar la posición del rover y eliminar rover

--- central/lib/central/rover.ex
+++ central/lib/central/rover.ex
@@ -1,4 +1,5 @@
 defmodule Central.Rover do
+  alias Central.Rover
   alias Central.Repo
   alias Central.Rover.Rover
 
@@ -6,5 +7,20 @@ defmodule Central.Rover do
     %Rover{}
     |> Rover.changeset(attrs)
     |> Repo.insert()
+  end
+
+  # Función para traer solo un rover basado en su id nil si no existe el rover con ese id
+  def get_rover(id) do
+    Repo.get(Rover, id)
+  end
+
+  def update_rover(%Rover{} = rover, attrs) do
+    rover
+    |> Rover.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_rover(id) do
+    Repo.delete(id)
   end
 end
