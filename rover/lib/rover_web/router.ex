1def1dde77e6a6a0ddca31097e2358b5de7e2940
Gerardo <gerardohuerta1502@gmail.com>
2026-08-17 09:57:29 -0600

    feat: rutas para servicio
    Se añaden rutas para cargar registros
    ruta para extraer un registro random

---
+++ rover/lib/rover_web/router.ex
@@ -0,0 +1,33 @@
+defmodule RoverWeb.Router do
+  alias Rover.Eventos
+  alias Rover.Vehiculo
+  use RoverWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", RoverWeb do
+    pipe_through :api
+
+    get "/load_eventos_db", Eventos, :load_eventos_db_controller
+    get "/get_evento_random", Eventos, :get_evento_random_controller
+  end
+
+  # Enable LiveDashboard and Swoosh mailbox preview in development
+  if Application.compile_env(:rover, :dev_routes) do
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    import Phoenix.LiveDashboard.Router
+
+    scope "/dev" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      live_dashboard "/dashboard", metrics: RoverWeb.Telemetry
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
