f43d51a83468b4d869dce3b72089a7e135ae97e0
Gerardo <gerardohuerta1502@gmail.com>
2026-08-17 09:55:16 -0600

    feat: schemas mejora y vehiculo

---
+++ rover/lib/rover/vehiculo/mejora.ex
@@ -0,0 +1,21 @@
+defmodule Rover.Vehiculo.Mejora do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "Mejora" do
+    field :name, :string, default: ""
+    field :description, :string, default: ""
+    field :puntaje, :float, default: 0.0
+
+    belongs_to(:rover, Rover.Vehiculo.Vehiculo, foreign_key: :id_rover)
+  end
+
+  def changeset(mejora, attrs) do
+    mejora
+    |> cast(attrs, [:name, :description, :puntaje, :id_rover])
+    |> validate_required([:name, :id_rover])
+    |> foreign_key_constraint(:id_rover)
+  end
+end
---
+++ rover/lib/rover/vehiculo/vehiculo.ex
@@ -0,0 +1,30 @@
+defmodule Rover.Vehiculo.Vehiculo do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "Vehiculo" do
+    field :pos_x, :integer, default: 0
+    field :pos_y, :integer, default: 0
+    field :timestamp, :utc_datetime
+
+    # Conexión con mejoras.
+    has_many :mejora, Rover.Vehiculo.Mejora, foreign_key: :id_rover
+    timestamps()
+  end
+
+  def changeset(rover, attrs) do
+    rover
+    |> cast(attrs, [:pos_x, :pos_y, :timestamp])
+    |> validate_required([:pos_x, :pos_y])
+  end
+
+  def update_position_changeset(rover, attrs) do
+    rover
+    |> cast(attrs, [:pos_x, :pos_y, :timestamp])
+    |> validate_required([:pos_x, :pos_y])
+    |> put_change(:timestamp, DateTime.truncate(DateTime.utc_now(), :second))
+  end
+end
