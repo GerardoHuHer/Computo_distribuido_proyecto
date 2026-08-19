d0291785862b9f3aaafdd22e69c0a51c9b0fac66
Gerardo <gerardohuerta1502@gmail.com>
2026-08-18 21:18:09 -0600

    feat: Vistas para el proyecto Rover

---
+++ rover/lib/rover_web/controllers/vehiculo/vehiculo_json.ex
@@ -0,0 +1,29 @@
+defmodule RoverWeb.VehiculoJSON do
+  alias Rover.Vehiculo.Vehiculo
+
+  def show(%{rover: rover}) do
+    %{data: data(rover)}
+  end
+
+  def index(%{rovers: list}) when is_list(list) do
+    %{data: for(rover <- list, do: data(rover))}
+  end
+
+  def error(%{changeset: changeset}) do
+    %{errors: Ecto.Changeset.traverse_errors(changeset, &translate_error/1)}
+  end
+
+  defp data(%Vehiculo{} = rover) do
+    %{
+      id: rover.id,
+      pos_x: rover.pos_x,
+      pos_y: rover.pos_y
+    }
+  end
+
+  defp translate_error({msg, opts}) do
+    Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+      opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+    end)
+  end
+end
