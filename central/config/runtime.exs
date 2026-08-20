fff562f777e850cf9d9725b3e0103065bedf2e4a
Gerardo <gerardohuerta1502@gmail.com>
2026-08-20 00:46:12 -0600

    Se cambia configuración para poder correr todo con las variables del .env

--- central/config/runtime.exs
+++ central/config/runtime.exs
@@ -21,7 +21,7 @@ if System.get_env("PHX_SERVER") do
 end
 
 config :central, CentralWeb.Endpoint,
-  http: [port: String.to_integer(System.get_env("PORT", "4000"))]
+  http: [port: String.to_integer(System.get_env("BACKEND_PORT", "4000"))]
 
 config :central, Central.Repo,
   username: System.get_env("POSTGRES_USER") || "user",
