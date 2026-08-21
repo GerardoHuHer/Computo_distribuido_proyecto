938b7a352f7d164aefaabcb0007d46c185fc820e
Gerardo <gerardohuerta1502@gmail.com>
2026-08-20 21:30:49 -0600

    fix: Se solucionan discrepancias de nombres en los test de los controladores

--- rover/test/rover_web/controllers/eventos_controller_test.exs
+++ rover/test/rover_web/controllers/eventos_controller_test.exs
@@ -1,5 +1,5 @@
 defmodule RoverWeb.EventosControllerTest do
-  use Rover.Conncase, async: true
+  use RoverWeb.ConnCase, async: true
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -7,15 +7,21 @@ defmodule RoverWeb.EventosControllerTest do
 
   describe "GET /api/load_eventos_db" do
     test "load_eventos_db_controller para la carga de 100 registros exitoso", %{conn: conn} do
-      conn = get("/api/load_eventos_db")
+      conn = get(conn, "/api/load_eventos_db")
       assert json_response(conn, 200) == %{"msg" => "Se han añadido los 100 eventos con éxito"}
     end
   end
 
   describe "GET /api/get_evento_random" do
     test "get_evento_random al obtener un registro", %{conn: conn} do
-      conn = get("/api/get_evento_random")
-      assert json_response(conn, 200)["data"] = %{id, name, description}
+      conn = get(conn, "/api/get_evento_random")
+
+      assert %{"data" => %{"id" => id, "name" => name, "description" => description}} =
+               json_response(conn, 200)
+
+      assert is_integer(id)
+      assert is_binary(name)
+      assert is_binary(description)
     end
   end
 end
--- rover/test/rover_web/controllers/vehicle_controller_test.exs
+++ rover/test/rover_web/controllers/vehicle_controller_test.exs
@@ -16,9 +16,9 @@ defmodule RoverWeb.VehiculoControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  describe "POST /api/create_rover (create)" do
+  describe "POST /api/create_vehiculo (create)" do
     test "test con HTTP 201, lo crea cuando los datos son válidos", %{conn: conn} do
-      conn = post(conn, ~p"/api/create_rover", data: @valid_attrs)
+      conn = post(conn, ~p"/api/create_vehiculo", data: @valid_attrs)
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
       assert json_response(conn, 201)["data"] == %{
@@ -31,76 +31,76 @@ defmodule RoverWeb.VehiculoControllerTest do
     test "test con HTTP 422, no se pudo procesar la entidad, los datos son inválidos", %{
       conn: conn
     } do
-      conn = post(conn, ~p"/api/create_rover", data: @invalid_attrs)
+      conn = post(conn, ~p"/api/create_vehiculo", data: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
 
-  describe "GET /api/get_rover/:id (READ)" do
+  describe "GET /api/get_vehiculo/:id (READ)" do
     test "test con HTTP 200 para leer un rover de la base de datos y devolverlo", %{conn: conn} do
-      {:ok, rover} = Rover.Vehiculo.create_rover(@valid_attrs)
+      {:ok, rover} = Rover.Vehiculo.create_vehiculo(@valid_attrs)
 
-      conn = get(conn, ~p"/api/get_rover/#{rover.id}")
+      conn = get(conn, ~p"/api/get_vehiculo/#{rover.id}")
 
       assert json_response(conn, 200)["data"]["id"] == rover.id
     end
 
     test "test con HTTP 404 para buscar un rover que no existe", %{conn: conn} do
-      {:ok, rover} = Rover.Vehiculo.create_rover(@valid_attrs)
-      Rover.Vehiculo.delete_rover(rover)
+      {:ok, rover} = Rover.Vehiculo.create_vehiculo(@valid_attrs)
+      Rover.Vehiculo.delete_vehiculo(rover)
 
-      conn = get(conn, ~p"/api/get_rover/#{rover.id}")
+      conn = get(conn, ~p"/api/get_vehiculo/#{rover.id}")
       assert json_response(conn, 404) == %{"error" => "Rover not found"}
     end
   end
 
-  describe "UPDATE /api/move_rover/:id" do
+  describe "UPDATE /api/move_vehiculo/:id" do
     test "test con HTTP 200 para hacer un cambio en la posición del rover", %{conn: conn} do
-      {:ok, rover} = Rover.Vehiculo.create_rover(@valid_attrs)
+      {:ok, rover} = Rover.Vehiculo.create_vehiculo(@valid_attrs)
 
-      conn = patch(conn, ~p"/api/move_rover/#{rover.id}", data: %{pos_x: 1})
+      conn = patch(conn, ~p"/api/move_vehiculo/#{rover.id}", data: %{pos_x: 1})
       assert json_response(conn, 200)["data"]["pos_x"] == 1
     end
 
     test "test con HTTP 404 en caso de que el rover que se quiere modificar no exista", %{
       conn: conn
     } do
-      {:ok, rover} = Rover.Vehiculo.create_rover(@valid_attrs)
-      Rover.Vehiculo.delete_rover(rover)
+      {:ok, rover} = Rover.Vehiculo.create_vehiculo(@valid_attrs)
+      Rover.Vehiculo.delete_vehiculo(rover)
 
-      conn = patch(conn, ~p"/api/move_rover/#{rover.id}", data: %{pos_x: 1})
+      conn = patch(conn, ~p"/api/move_vehiculo/#{rover.id}", data: %{pos_x: 1})
       assert json_response(conn, 404) == %{"error" => "Rover not found"}
     end
 
     test "test en caso que de que haya un error en el changeset", %{conn: conn} do
-      {:ok, rover} = Rover.Vehiculo.create_rover(@valid_attrs)
+      {:ok, rover} = Rover.Vehiculo.create_vehiculo(@valid_attrs)
 
-      conn = patch(conn, ~p"/api/move_rover/#{rover.id}", data: @invalid_attrs)
+      conn = patch(conn, ~p"/api/move_vehiculo/#{rover.id}", data: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
 
-  describe "DELETE /api/delete_rover/:id" do
+  describe "DELETE /api/delete_vehiculo/:id" do
     test "Eliminar con http 200 un rover basado en su id que sí exista", %{conn: conn} do
-      {:ok, rover} = Rover.Vehiculo.create_rover(@valid_attrs)
-      conn = delete(conn, ~p"/api/delete_rover/#{rover.id}")
+      {:ok, rover} = Rover.Vehiculo.create_vehiculo(@valid_attrs)
+      conn = delete(conn, ~p"/api/delete_vehiculo/#{rover.id}")
       assert json_response(conn, 200) == %{"msg" => "Rover #{rover.id} lost connection"}
-      assert Rover.Vehiculo.get_rover(rover.id) == nil
+      assert Rover.Vehiculo.get_vehiculo(rover.id) == nil
     end
 
     test "Eliminar con con http 404 un rover que no existe", %{conn: conn} do
-      {:ok, rover} = Rover.Vehiculo.create_rover(@valid_attrs)
-      Rover.Vehiculo.delete_rover(rover)
+      {:ok, rover} = Rover.Vehiculo.create_vehiculo(@valid_attrs)
+      Rover.Vehiculo.delete_vehiculo(rover)
 
-      conn = delete(conn, ~p"/api/delete_rover/#{rover.id}")
+      conn = delete(conn, ~p"/api/delete_vehiculo/#{rover.id}")
       assert json_response(conn, 404) == %{"error" => "Rover #{rover.id} not found"}
     end
   end
 
-  describe "GET /api/get_all_rovers" do
+  describe "GET /api/get_all_vehiculos" do
     test "Traer todos los rovers con HTTP 200 con al menos un elemento", %{conn: conn} do
-      {:ok, rover} = Rover.Vehiculo.create_rover(@valid_attrs)
-      conn = get(conn, ~p"/api/get_all_rovers")
+      {:ok, rover} = Rover.Vehiculo.create_vehiculo(@valid_attrs)
+      conn = get(conn, ~p"/api/get_all_vehiculos")
 
       assert json_response(conn, 200)["data"] == [
                %{
@@ -114,7 +114,7 @@ defmodule RoverWeb.VehiculoControllerTest do
     end
 
     test "Traer todos los rovers con HTTP 200 sin elementos", %{conn: conn} do
-      conn = get(conn, ~p"/api/get_all_rovers")
+      conn = get(conn, ~p"/api/get_all_vehiculos")
       assert json_response(conn, 200)["data"] == []
       assert Enum.count(json_response(conn, 200)["data"]) == 0
     end
