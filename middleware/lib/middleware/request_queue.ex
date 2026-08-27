566533d4098898fc7807d67713dfedc9f8e9021f
Gerardo <gerardohuerta1502@gmail.com>
2026-08-26 23:30:49 -0600

    feat: Servicio de "encolamiento".
    Actualmente no funciona con una cola, eso se habilitará una vez que se
    tenga varios backends del mismo para determinar disponibilidad

---
+++ middleware/lib/middleware/request_queue.ex
@@ -0,0 +1,41 @@
+defmodule Middleware.RequestQueue do
+  use GenServer
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(_opts) do
+    {:ok, :queue.new()}
+  end
+
+  def handle_call({:encolar, payload}, _from, estado) do
+    resultado = procesar(payload)
+    {:reply, resultado, estado}
+  end
+
+  defp procesar(%{"type" => type, "action" => action, "params" => params}) do
+    prefijo = "backend_#{type}"
+
+    case buscar_nodo(prefijo) do
+      {:ok, nodo} ->
+        case :rpc.call(nodo, RpcHandler, :handle_request, [action, params], 5000) do
+          {:badrpc, reason} -> {:error, {:badrpc, reason}}
+          resultado -> resultado
+        end
+
+      {:error, :no_nodes} ->
+        {:error, :no_backend_disponible}
+    end
+  end
+
+  # TODO: Mejorar esta función a largo plazo para encontrar el nodo con mayor disponibilidad y para usar la cola
+  defp buscar_nodo(prefijo) do
+    Node.list()
+    |> Enum.filter(fn nodo -> String.starts_with?(Atom.to_string(nodo), prefijo) end)
+    |> case do
+      [] -> {:error, :no_nodes}
+      [nodo | _resto] -> {:ok, nodo}
+    end
+  end
+end
