34f9d0dc2accb06a5b59ec39759ee8acf550007c
Gerardo <gerardohuerta1502@gmail.com>
2026-08-26 22:17:47 -0600

    feat: cambios en el script entrypoint para poderse levantar con libcluster

---
+++ middleware/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "Creando base de datos"
+mix ecto.create
+
+echo "Corriendo migraciones"
+mix ecto.migrate
+
+echo "Levantando Phoenix"
+NODE_IP=$(hostname -i)
+exec elixir --name backend_rover@${NODE_IP} --cookie ${RELEASE_COOKIE} -S mix phx.server
