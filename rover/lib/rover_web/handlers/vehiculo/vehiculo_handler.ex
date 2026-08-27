ec09636b1f66fab5d987c2289cc04de26a5d9ada
Gerardo <gerardohuerta1502@gmail.com>
2026-08-27 14:30:49 -0600

    fix: Se arregla json respuesta para que se pueda procesar

--- rover/lib/rover_web/handlers/vehiculo/vehiculo_handler.ex
+++ rover/lib/rover_web/handlers/vehiculo/vehiculo_handler.ex
@@ -4,7 +4,12 @@ defmodule RoverWeb.Handlers.VehiculoHandler do
   def handle_request("create_rover", params) do
     case Vehiculo.create_vehiculo(params) do
       {:ok, rover} ->
-        {:ok, %{rover: rover}}
+        {:ok,
+         %{
+           id: rover.id,
+           pos_x: rover.pos_x,
+           pos_y: rover.pos_y
+         }}
 
       {:error, changeset} ->
         {:error, %{changeset: changeset}}
