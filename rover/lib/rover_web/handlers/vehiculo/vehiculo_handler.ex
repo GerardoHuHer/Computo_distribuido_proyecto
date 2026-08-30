679f4a576fdda72b39a9ec435b96f977a76fab56
Gerardo <gerardohuerta1502@gmail.com>
2026-08-29 20:54:50 -0600

    feat: Handlers para hacer lectura a todos los rovers y para uno solo por id

--- rover/lib/rover_web/handlers/vehiculo/vehiculo_handler.ex
+++ rover/lib/rover_web/handlers/vehiculo/vehiculo_handler.ex
@@ -4,15 +4,33 @@ defmodule RoverWeb.Handlers.VehiculoHandler do
   def handle_request("create_rover", params) do
     case Vehiculo.create_vehiculo(params) do
       {:ok, rover} ->
-        {:ok,
-         %{
-           id: rover.id,
-           pos_x: rover.pos_x,
-           pos_y: rover.pos_y
-         }}
+        {:ok, data(rover)}
 
       {:error, changeset} ->
         {:error, %{changeset: changeset}}
     end
+  end
+
+  def handle_request("get_all_vehiculos", _params) do
+    vehiculos = Vehiculo.get_all_vehiculos()
+    {:ok, for(vehiculo <- vehiculos, do: data(vehiculo))}
+  end
+
+  def handle_request("get_vehiculo", params) do
+    case Vehiculo.get_vehiculo(params["id"]) do
+      nil ->
+        {:error, %{error: "Rover not found"}}
+
+      rover ->
+        {:ok, data(rover)}
+    end
+  end
+
+  defp data(%Rover.Vehiculo.Vehiculo{} = vehiculo) do
+    %{
+      id: vehiculo.id,
+      pos_x: vehiculo.pos_x,
+      pos_y: vehiculo.pos_y
+    }
   end
 end
