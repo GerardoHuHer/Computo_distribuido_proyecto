311eb0cd7eb4130b4028f0574ea5bd5e82db7acb
Gerardo <gerardohuerta1502@gmail.com>
2026-09-10 11:47:54 -0600

    feat: Schema for eventos in central

---
+++ central/lib/central/eventos/eventos.ex
@@ -0,0 +1,19 @@
+defmodule Central.Eventos.Eventos do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "eventos" do
+    field :name, :string, default: ""
+    field :description, :string, default: ""
+    field :pos_x, :integer
+    field :pos_y, :integer
+
+    timestamps()
+  end
+
+  def changeset(eventos, attrs) do
+    eventos
+    |> cast(attrs, [:name, :description, :pos_x, :pos_y])
+    |> validate_required([:name, :description, :pos_x, :pos_y])
+  end
+end
