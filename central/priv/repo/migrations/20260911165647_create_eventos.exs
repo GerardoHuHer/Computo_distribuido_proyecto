5545e4419c3ff1bd692ce415775e22dca119c2dc
Gerardo <gerardohuerta1502@gmail.com>
2026-09-11 11:04:17 -0600

    feat: create_eventos migration for eventos in central

---
+++ central/priv/repo/migrations/20260911165647_create_eventos.exs
@@ -0,0 +1,16 @@
+defmodule Central.Repo.Migrations.CreateEventos do
+  use Ecto.Migration
+
+  def change do
+    create_table(:eventos) do
+      add :id, :integer, primary_key: true
+      add :name, :string, default: ""
+      add :description, :string, default: ""
+      add :pos_x, :integer
+      add :pos_y, :integer
+      add :timestamp, :utc_datetime
+
+      timestamps()
+    end
+  end
+end
