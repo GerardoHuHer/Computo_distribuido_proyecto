3b604c5cc9e23a638414a88680686eff3cdf1f05
Gerardo <gerardohuerta1502@gmail.com>
2026-09-12 12:16:47 -0600

    feat: capa de negocios para central

--- central/lib/central/eventos.ex
+++ central/lib/central/eventos.ex
@@ -2,9 +2,31 @@ defmodule Central.Eventos do
   alias Central.Repo
   alias Central.Eventos.Eventos
 
+  # Create a event 
   def post_evento_pos(attrs \\ %{}) do
     %Eventos{}
     |> Eventos.changeset(attrs)
     |> Repo.insert()
+  end
+
+  # Read all events
+  def read_all_eventos() do
+    Repo.all(Eventos)
+  end
+
+  def get_evento(id) do
+    Repo.get(Eventos, id)
+  end
+
+  # Change the revisado field.
+  def update_revisado(%Eventos{} = evento, attrs) do
+    evento
+    |> Eventos.changeset(attrs)
+    |> Repo.update()
+  end
+
+  # Delete an event
+  def delete_evetos(evento) do
+    Repo.delete(evento)
   end
 end
