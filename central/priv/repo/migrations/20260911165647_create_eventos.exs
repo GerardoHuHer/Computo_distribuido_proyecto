457fbbbc6859e4f160e772943ee353b650abcacc
Gerardo <gerardohuerta1502@gmail.com>
2026-09-12 12:20:28 -0600

    feat: Eventos migration was added

--- central/priv/repo/migrations/20260911165647_create_eventos.exs
+++ central/priv/repo/migrations/20260911165647_create_eventos.exs
@@ -8,6 +8,7 @@ defmodule Central.Repo.Migrations.CreateEventos do
       add :description, :string, default: ""
       add :pos_x, :integer
       add :pos_y, :integer
+      add :revisado, :boolean
       add :timestamp, :utc_datetime
 
       timestamps()
