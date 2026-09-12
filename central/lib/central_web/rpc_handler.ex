51c024931f1112b33579d789619a2365aea7214e
Gerardo <gerardohuerta1502@gmail.com>
2026-09-12 12:19:57 -0600

    feat: General handler were added

--- central/lib/central_web/rpc_handler.ex
+++ central/lib/central_web/rpc_handler.ex
@@ -5,6 +5,15 @@ defmodule CentralWeb.RpcHandler do
   def handle_request("recibir_evento", params),
     do: EventosHandler.handle_request("recibir_evento", params)
 
+  def handle_request("read_all_events", params),
+    do: EventosHandler.handle_request("read_all_events", params)
+
+  def handle_request("update_event", params),
+    do: EventosHandler.handle_request("update_event", params)
+
+  def handle_request("delete_event", params),
+    do: EventosHandler.handle_request("delete_event", params)
+
   def handle_request(unknow_action, _params) do
     {:error, {:unknow_action, unknow_action}}
   end
