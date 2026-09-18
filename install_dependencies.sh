ebe676580b0ce8e3495f27a6a7401cbf4e9aa004
Gerardo <gerardohuerta1502@gmail.com>
2026-09-18 16:07:07 -0600

    feat: Se añade make al script de instalación

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+
+echo "Las dependencias necesarias son: \n1. Docker \n2. Make "
 # Remove previous versions
 sudo dnf remove docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-selinux \
-                  docker-engine-selinux \
-                  docker-engine
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-selinux \
+  docker-engine-selinux \
+  docker-engine
 
 # Set up repositories
 sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
@@ -20,5 +22,8 @@ sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin dock
 # Start docker engine
 sudo systemctl enable --now docker
 
-#Check docker installation 
+#Check docker installation
 sudo docker run hello-world
+
+# Instalar make and gcc
+sudo dnf install make gcc
