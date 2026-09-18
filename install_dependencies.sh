3c173ac4e84adf1a0ec3c147c6fff809d33dec73
Gerardo <gerardohuerta1502@gmail.com>
2026-09-18 16:04:55 -0600

    feat: Install dependencies script

---
+++ install_dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Remove previous versions
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+# Set up repositories
+sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+
+# Install docker engine
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Start docker engine
+sudo systemctl enable --now docker
+
+#Check docker installation 
+sudo docker run hello-world
